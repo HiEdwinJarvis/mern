66db6d5662c68b414e77096911bdc14d801313d1
“zhangqi <1973926163@qq.com>
2019-06-17 17:08:04 +0800

    2.4ES2015

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
-var contentNode = document.getElementById('contents'); 
-var component = <h1>Hello World!撒大声地算法</h1>;   // A simple JSX component 
-ReactDOM.render(component, contentNode);   //  Render the component inside  the content Node
+const contentNode = document.getElementById('contents');
+const continents = ['Africa','America','Asia','Australia','Europe'];
+ const message = continents.map(c => `Hello ${c}!`).join(' ');
+const component = <p>{message}</p>;      // A simple JSX component 
+ReactDOM.render(component, contentNode);  //  Render the component inside the  content Node 
