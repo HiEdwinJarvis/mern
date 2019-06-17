95d8852a6b7acdb36188d3ca09629b21e11c6104
“zhangqi <1973926163@qq.com>
2019-06-17 16:54:07 +0800

    2.3.3-自动化

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 var contentNode = document.getElementById('contents'); 
-var component = <h1>Hello World!</h1>;   // A simple JSX component 
+var component = <h1>Hello World!撒大声地算法</h1>;   // A simple JSX component 
 ReactDOM.render(component, contentNode);   //  Render the component inside  the content Node
--- static/App.js
+++ static/App.js
@@ -2,6 +2,6 @@ var contentNode = document.getElementById('contents');
 var component = React.createElement(
   'h1',
   null,
-  'Hello World!'
+  'Hello World!\u6492\u5927\u58F0\u5730\u7B97\u6CD5'
 ); // A simple JSX component 
 ReactDOM.render(component, contentNode); //  Render the component inside  the content Node
