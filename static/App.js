0275cbd930600e37d6e712e53f18e72aea4ff630
“zhangqi <1973926163@qq.com>
2019-06-17 17:16:23 +0800

    3.3-组件组装

--- static/App.js
+++ static/App.js
@@ -1,10 +1,47 @@
 const contentNode = document.getElementById('contents');
+class IssueFilter extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'This is a placeholder for the Issue Filter.'
+    );
+  }
+}
+class IssueTable extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'This is a placeholder for a table of Issues.'
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'This is a placeholder for an Issue Add entry form.'
+    );
+  }
+}
 class IssueList extends React.Component {
   render() {
     return React.createElement(
       'div',
       null,
-      'This is a placeholder for the issue list.'
+      React.createElement(
+        'h1',
+        null,
+        'Issue Tracker'
+      ),
+      React.createElement(IssueFilter, null),
+      React.createElement('hr', null),
+      React.createElement(IssueTable, null),
+      React.createElement('hr', null),
+      React.createElement(IssueAdd, null)
     );
   }
 }
