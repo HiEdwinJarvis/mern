0a784ae7b027e8b0e21d7fad5da5dc322db2f096
“zhangqi <1973926163@qq.com>
2019-06-21 14:25:44 +0800

    6.5-向mongodb写数据

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ MongoClient.connect('mongodb://localhost/issuetracker').then(connection => {
   console.log('ERROR:', error);
 });
 
-const issues = [  {
-    id: 1, status: 'Open', owner: 'Ravan',
-	created: new Date('2016-08-15'), effort: 5, completionDate: undefined,    title: 'Error in console when clicking Add',  },
-	{    id: 2, status: 'Assigned', owner: 'Eddie',     created: new Date('2016-08-16'), effort: 14, 
-	completionDate: new Date('2016-08-30'),    title: 'Missing bottom border on panel',  }, ];
+
 app.get('/api/issues', (req, res) => { 
  db.collection('issues').find().toArray().then(issues => {
 	 const metadata = { total_count: issues.length };
@@ -29,14 +25,21 @@ app.get('/api/issues', (req, res) => {
  
 
 const validIssueStatus = {  New: true,  Open: true,  Assigned: true,  Fixed: true,  Verified: true,  Closed: true, };
-const issueFieldType = {  id: 'required',  status: 'required',  owner: 'required',  effort: 'optional',  created: 'required',  completionDate: 'optional',  title: 'required', };
+const issueFieldType = {  
+  status: 'required',  owner: 'required',  effort: 'optional',  created: 'required',  completionDate: 'optional',  title: 'required', };
 function validateIssue(issue) {  for (const field in issueFieldType) {    const type = issueFieldType[field];    if (!type) {      delete issue[field];    } else if (type === 'required' && !issue[field]) {      return `${field} is required.`;    }  }
   if (!validIssueStatus[issue.status])    return `${issue.status} is not a valid status.`;
   return null; }
-app.post('/api/issues', (req, res) => {  const newIssue = req.body;  newIssue.id = issues.length + 1;  newIssue.created = new Date();  if (!newIssue.status)    newIssue.status = 'New';
-  const err = validateIssue(newIssue)
+app.post('/api/issues', (req, res) => {
+	const newIssue = req.body;
+	 newIssue.created = new Date();  if (!newIssue.status)    newIssue.status = 'New';
+  const err = validateIssue(newIssue)  
   if (err) {
-	  res.status(422).json({ message: `Invalid requrest: ${err}` });    return;
-  }  issues.push(newIssue);
-  res.json(newIssue); });
-
+	  res.status(422).json({ message: `Invalid request: ${err}` });    return; 
+	  }
+  db.collection('issues').insertOne(newIssue).then(result =>    db.collection('issues').find({ _id: result.insertedId }).limit(1).next()  ).then(newIssue => { 
+  res.json(newIssue);  
+  }).catch(error => {  
+  console.log(error);   
+  res.status(500).json({ message: `Internal Server Error: ${error}` }); 
+  }); });
